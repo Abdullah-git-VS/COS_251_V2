fb9e10b224bb2866feb686d8413b3198ba452c26
Abdullah-git-VS <73521453+Abdullah-git-VS@users.noreply.github.com>
2025-03-30 17:04:08 +0300

    s

--- main.php
+++ main.php
@@ -8,7 +8,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color:rgb(36, 201, 193);
+            background-color:rgb(201, 44, 36);
             display: flex;
             justify-content: center;
             align-items: center;
