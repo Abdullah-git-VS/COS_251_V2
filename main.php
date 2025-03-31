696f01f6e1815a2268eafa11b3f0915fd75a11cb
Abdullah-git-VS <205581067+dsgdd3@users.noreply.github.com>
2025-03-31 08:51:51 +0300

    Update main.php

--- main.php
+++ main.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
     <style>
-	---dsds--
         body {
             font-family: Arial, sans-serif;
             background-color:rgb(36, 132, 201);
