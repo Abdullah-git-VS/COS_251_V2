ab91e15be471fc91dcf34fec9d7ea4891a3a8e14
Abdullah-git-VS <205581067+dsgdd3@users.noreply.github.com>
2025-04-01 05:38:37 +0300

    1

--- main.php
+++ main.php
@@ -21,6 +21,7 @@
             border-radius: 8px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
             width: 300px;
+            
         }
         .signin-container h2 {
             margin-bottom: 20px;
