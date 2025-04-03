f1efa5481e757b4539264ad79935117d37e10b04
Abdullah-git-VS <73521453+Abdullah-git-VS@users.noreply.github.com>
2025-04-03 10:33:44 +0300

    Add files via upload

---
+++ index-2.php
@@ -0,0 +1,36 @@
+<DOCTYPE html>
+
+<html>
+
+<head>
+<title> my project </title>
+<link rel="stylesheet" href="index2.php">
+</head>
+
+<body>
+
+<div class="container">
+<header>
+<a href="#" class="logo"> transport </a>
+
+<ul>
+<li> <a href="#"> registration </a> </li>
+<li> <a href="#"> order services </a> </li>
+<li> <a href="#"> accessories </a> </li>
+<li> <a href="#"> about </a> </li>
+<li> <a href="#"> profile </a> </li>
+</ul>
+
+</header>
+
+<div class="content">
+<h2> Transportation services </h2>
+<p> we focus on providing safe, reliable, and stress-free transportation for animals of all kinds. whether you need to relocate a pet, transport livestock or move exotic animals</p>
+<button> click for more </button>
+
+
+</div>
+
+
+</body>
+</html>
