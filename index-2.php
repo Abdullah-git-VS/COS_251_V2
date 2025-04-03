67a8c66bf05e93ab8ed2f7240abf54f3621230d1
Abdullah-git-VS <205581067+dsgdd3@users.noreply.github.com>
2025-04-03 11:05:33 +0300

    2

--- index-2.php
+++ index-2.php
@@ -1,36 +1,36 @@
 <DOCTYPE html>
+    <html>
 
-<html>
+    <head>
+        <title> my project </title>
+        <link rel="stylesheet" href="index2.css">
+    </head>
 
-<head>
-<title> my project </title>
-<link rel="stylesheet" href="index2.css">
-</head>
+    <body>
 
-<body>
+        <div class="container">
+            <header>
+                <a href="#" class="logo"> transport </a>
 
-<div class="container">
-<header>
-<a href="#" class="logo"> transport </a>
+                <ul>
+                    <li> <a href="#"> registration </a> </li>
+                    <li> <a href="#"> order services </a> </li>
+                    <li> <a href="#"> accessories </a> </li>
+                    <li> <a href="#"> about </a> </li>
+                    <li> <a href="#"> profile </a> </li>
+                </ul>
 
-<ul>
-<li> <a href="#"> registration </a> </li>
-<li> <a href="#"> order services </a> </li>
-<li> <a href="#"> accessories </a> </li>
-<li> <a href="#"> about </a> </li>
-<li> <a href="#"> profile </a> </li>
-</ul>
+            </header>
 
-</header>
-
-<div class="content">
-<h2> Transportation services </h2>
-<p> we focus on providing safe, reliable, and stress-free transportation for animals of all kinds. whether you need to relocate a pet, transport livestock or move exotic animals</p>
-<button> click for more </button>
-
-
-</div>
+            <div class="content">
+                <h2> Transportation services </h2>
+                <p> we focus on providing safe, reliable, and stress-free transportation for animals of all kinds. whether you need to relocate a pet, transport livestock or move exotic animals</p>
+                <button> click for more </button>
 
 
-</body>
-</html>
+            </div>
+
+
+    </body>
+
+    </html>
