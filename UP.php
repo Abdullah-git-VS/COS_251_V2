46ca8230adf596c80adf458207592fef796b54a9
Abdullah-git-VS <205581067+dsgdd3@users.noreply.github.com>
2025-04-12 17:29:58 +0300

    FINISH

--- UP.php
+++ UP.php
@@ -9,14 +9,12 @@ if (isset($_POST['update'])) {
     $IMAGE = $_FILES['image'];
     $image_location = $_FILES['image']['tmp_name'];
     $image_name = $_FILES['image']['name'];
+    move_uploaded_file($image_location,'images/'.$image_name);
     $image_up = "images/".$image_name;
     $update = "UPDATE prod SET name='$NAME' , price='$PRICE', image='$image_up', id='$ID_n' WHERE id=$ID_o";
     mysqli_query($con, $update);
 
-    if (move_uploaded_file($image_location, 'images/'.$image_name)) {
-        echo "<script>alert('تم تحديث المنتج بنجاح');</script>";
-    } else {
-        echo "<script>alert('حدث خطأ ما يرجى المحاولة لاحقا');</script>";
-    }
+
     header('location: test.php');
 }
+?>
--- insert.php
+++ insert.php
@@ -10,11 +10,6 @@ if (isset($_POST['upload'])) {
     $image_up = "images/".$image_name;
     $insert = "INSERT INTO prod (name,price,image) VALUES('$NAME','$PRICE','$image_up')";
     mysqli_query($con, $insert);
-    if (move_uploaded_file($image_location, 'images/' . $image_name)) {
-        echo "<script>alert('تم رفع المنتج بنجاح');</script>";
-    } else {
-        echo "<script>alert('حدثة مشكلة لم يتم رفع المنتج بنجاح');</script>";
-    }
     mysqli_close($con);
 }
 header('location: test.php');
